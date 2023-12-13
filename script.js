a0ddbf350384c4cadea11a79b4011754ca6cf7c8
oriel <orielsilvajr@gmail.com>
2023-12-13 03:27:00 -0500

    not fun

--- script.js
+++ script.js
@@ -4,7 +4,23 @@ function myAlert() {
     alert('hello world')
 }
 
+let suggestions = ['The', 'button', 'has', 'been', 'clicked', 'several', 'times', 'now']
+let counter = 0;
+
 function signIn(btn) {
-    btn.innerText = 'sign-out'
-    
+    if (btn.innerText.toLowerCase() === 'sign-in') {
+        btn.innerText = 'sign-out';
+    } else {
+        btn.innerText = 'sign-in';
+    }
+
 }
+
+// function randomGenAlert() {
+//     for (let i = 0; i < suggestions.length; i++) {
+
+//         document.getElementById('homeLink').addEventListener('click', randomGenAlert);
+
+//     }
+
+// }
